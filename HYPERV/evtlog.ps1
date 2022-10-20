aad39809b2e7d19e77b5d27e055d34baa3c9750e
Erich <messenger.erich@hotmail.com>
2022-10-19 20:38:36 -0500

    added values for evtlog

--- HYPERV/evtlog.ps1
+++ HYPERV/evtlog.ps1
@@ -1,7 +1,7 @@
 #Variables
-$Logname = Read-host "What type of log do you want to see?"
+$Logname = Read-host "What type of log do you want to see? System, Application, Security"
 $number = Read-host "How many entries do you want to see?"
-$Type = Read-host "What type of entries do you want to see?"
+$Type = Read-host "What type of entries do you want to see? Valid Types are Error, Information, FailureAudit, SuccessAudit, Warning" 
 
 #CMDLETS
 Get-eventlog `
